042a1ebf00ded2832119ed38423ce0184e68e7cf
Chris Anderson <jchris@integrity.local>
2008-05-02 12:06:07 -0700

    basic specs passing

--- jquery.gels.js
+++ jquery.gels.js
@@ -1,4 +1,12 @@
 // credits and such
 ;(function($) {
-  
+  $.fn.gels = function(opts) {
+    opts = $.extend({parent:"auto"},opts||{});
+    $(this).filter("[class^=gel]").each(function() {
+      gelify(this);
+    });
+  };
+  function gelify() {
+    
+  };
 })(jQuery);
